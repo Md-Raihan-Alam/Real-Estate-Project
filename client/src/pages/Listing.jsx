e5c681dc36b2aa9dc43fac9089f51d72b1992071
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2024-12-09 18:39:52 +0600

    listing page ui design

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -5,12 +5,15 @@ import {Swiper,SwiperSlide} from "swiper/react";
 import SwiperCore from "swiper";
 import {Navigation} from "swiper/modules";
 import "swiper/css/bundle";
+import { FaBath, FaBed, FaChair, FaParking, FaShare } from "react-icons/fa";
+import { FaMapMarkerAlt } from "react-icons/fa";
 const Listing = () => {
     SwiperCore.use([Navigation]);
     const params=useParams();
     const [listing,setLising]=useState(null);
     const [loading,setLoading]=useState(false);
     const [error,setError]=useState(false);
+    const [copied,setCopied]=useState(false);
     useEffect(() => {
         const fetchListing = async () => {
             try {
@@ -25,6 +28,7 @@ const Listing = () => {
                     return ;
                 }
                 setLoading(false);
+                console.log(response.data);
                 setLising(response.data);
             } catch (error) {
                 setError(true);
@@ -48,6 +52,74 @@ const Listing = () => {
                 </SwiperSlide>
             ))}
         </Swiper>
+        <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+            <FaShare className="text-slate-500"
+            onClick={()=>{
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(()=>{
+                    setCopied(false);
+                },2000);
+            }}
+            />
+        </div>
+        {
+            copied && (
+                <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">
+                    Link copied!
+                </p>
+            )
+        }
+        <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
+            <p className="text-2xl font-semibold">
+                {listing.name} - ${" "}
+                {
+  listing.offer 
+    ? Number(listing.discountPrice).toLocaleString("en-US") 
+    : Number(listing.regularPrice).toLocaleString("en-US")
+
+}
+            </p>
+            <p className="flex items-center mt-6 gap-2 text-slate-600 my-2 text-sm">
+            <FaMapMarkerAlt className="text-green-700"/>
+            {listing.address}
+            </p>
+            <div className="flex gap-4">
+                <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                    {listing.type==="rent"?"For Rent":"For Sale"}
+                </p>
+                {listing.offer && (
+                    <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                        ${+listing.regularPrice - +listing.discountPrice}
+                    </p>
+                )}
+            </div>
+            <p className="text-slate-800">
+            <span className="font-semibold text-black">
+                Description - {" "}
+            </span>
+            {listing.description}
+            </p>
+            <ul className="flex flex-wrap items-center gap-4 sm:gap-6 text-green-900 font-semibold text-sm">
+                <li className="flex items-center gap-1 whitespace-nowrap">
+                    <FaBed  className="text-lg"/>
+                    {listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : `${listing.bedrooms} Bedroom`}
+                </li>
+                <li className="flex items-center gap-1 whitespace-nowrap">
+                    <FaBath  className="text-lg"/>
+                    {listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : `${listing.bathrooms} Bathrooms`}
+                </li>
+                <li className="flex items-center gap-1 whitespace-nowrap">
+                    <FaParking  className="text-lg"/>
+                    {listing.parking  ? `Parking spot` : `No Parking`}
+                </li>
+                <li className="flex items-center gap-1 whitespace-nowrap">
+                    <FaChair  className="text-lg"/>
+                    {listing.furnished > 1 ? `Furnished` : `Not Furnished`}
+                </li>
+            </ul>
+        </div>
+        
         </>
       )}
     </main>

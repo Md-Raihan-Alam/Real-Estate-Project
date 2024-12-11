855af4da7f034e50e82b17824e2ab90f631d0f83
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2024-12-11 19:12:56 +0600

    feat(api & ui): search api added & search page ui WIP

--- server/routes/listingRoutes.js
+++ server/routes/listingRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createListing,deleteListing,updateListing,getListing } = require("../controller/listingController");
+const { createListing,deleteListing,updateListing,getListing ,getListings} = require("../controller/listingController");
 const verifyToken = require("../utills/verifyUser");
 
 const router = express.Router();
@@ -8,4 +8,5 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id",getListing);
+router.get("/get",getListings);
 module.exports = router;

464a5679b4119a0da2b61f6556b496c368135dba
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2024-12-23 18:41:32 +0600

    api updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://real-estate-project-server.onrender.com/",
         secure: false,
       },
     },

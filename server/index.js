9c7682f58e3918bcd482e417734e9e2b07f4419b
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2025-01-01 00:30:07 +0600

    cor added

--- server/index.js
+++ server/index.js
@@ -6,18 +6,28 @@ const authRouter = require("./routes/authRoutes");
 const listingRouter = require("./routes/listingRoutes");
 const errorInfo = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
 dotenv.config();
-// mongoose.connect(
-//   ""
-// );
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Enable CORS for the specific client
+app.use(
+  cors({
+    origin: "https://real-estate-project-eosin-delta.vercel.app",
+    credentials: true, // Allow cookies to be sent with the request
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 app.use(errorInfo);
+
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
@@ -28,4 +38,5 @@ const start = async () => {
     console.log(error);
   }
 };
+
 start();

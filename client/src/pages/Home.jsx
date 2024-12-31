8051743cd943d4fc62bb139cf472870006f8b407
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2025-01-01 00:35:49 +0600

    api updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,82 +1,85 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import ListingItems from '../components/ListingItems';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import {Swiper,SwiperSlide} from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
-import {Navigation} from "swiper/modules";
+import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
+
 export default function Home() {
   const [offerListings, setOfferListings] = useState([]);
-  const [saleListings,setSaleListings] = useState([]);
-  const [rentListings,setRentListings]=useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
   SwiperCore.use([Navigation]);
+
   useEffect(() => {
-    const fetchOfferListings=async()=>{
-      try{
-        const {data} = await axios.get('/api/listing/get?offer=true&limit=4');
+    const fetchOfferListings = async () => {
+      try {
+        const { data } = await axios.get('https://real-estate-project-server.onrender.com/api/listing/get?offer=true&limit=4');
         setOfferListings(data);
         fetchRentListings();
-      }catch(error)
-      {
+      } catch (error) {
         console.log(error);
       }
     }
-    const fetchRentListings=async()=>{
-      try{
-        const {data} = await axios.get('/api/listing/get?type=rent&limit=4');
+
+    const fetchRentListings = async () => {
+      try {
+        const { data } = await axios.get('https://real-estate-project-server.onrender.com/api/listing/get?type=rent&limit=4');
         setRentListings(data);
         fetchSaleListings();
-      }catch(error)
-      { 
+      } catch (error) {
         console.log(error);
       }
     }
-    const fetchSaleListings=async()=>{
-      try{
-        const {data} = await axios.get('/api/listing/get?type=sale&limit=4');
+
+    const fetchSaleListings = async () => {
+      try {
+        const { data } = await axios.get('https://real-estate-project-server.onrender.com/api/listing/get?type=sale&limit=4');
         setSaleListings(data);
-      }catch(error)
-      {
+      } catch (error) {
         console.log(error);
       }
     }
+
     fetchOfferListings();
   }, []);
+
   return (
     <div>
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
-          Find your next <span className="text-slate-500">perfect</span><br/> place with ease
+          Find your next <span className="text-slate-500">perfect</span><br /> place with ease
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
-          REAL ESTATE is the best place to find yoour next perfect place to live<br/>
-          With over 1 million homes for sale available on the website, REAL ESTATE can match you with a house you will want to call home. 
-        </div> 
+          REAL ESTATE is the best place to find your next perfect place to live<br />
+          With over 1 million homes for sale available on the website, REAL ESTATE can match you with a house you will want to call home.
+        </div>
         <Link to={"/search"} className="text-xs sm:text-sm text-blue-600 font-bold hover:underline">
           Let&apos;s get started
         </Link>
       </div>
-      <Swiper navigation={true} >
-      {
-        offerListings && offerListings.length>0 && offerListings.map((listing)=>(
-          <SwiperSlide key={listing._id}>
-              <div 
+      <Swiper navigation={true}>
+        {
+          offerListings && offerListings.length > 0 && offerListings.map((listing) => (
+            <SwiperSlide key={listing._id}>
+              <div
                 style={{
-                  backgroundImage: `url("${listing.imageUrls[0]}")`, 
-                  backgroundPosition: "center", 
-                  backgroundRepeat: "no-repeat", 
+                  backgroundImage: `url("${listing.imageUrls[0]}")`,
+                  backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
                   backgroundSize: "cover"
-                }} 
+                }}
                 className="h-[500px]"
               ></div>
             </SwiperSlide>
-        ))
-      }
+          ))
+        }
       </Swiper>
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
         {
-          offerListings && offerListings.length>0 && (
+          offerListings && offerListings.length > 0 && (
             <div className="">
               <div className="my-3">
                 <h2 className='text-2xl font-semibold text-slate-600'>Recent offers</h2>
@@ -86,8 +89,8 @@ export default function Home() {
               </div>
               <div className="flex flex-wrap gap-4">
                 {
-                  offerListings. map((listing)=>(
-                    <ListingItems  listing={listing} key={listing._id}/>
+                  offerListings.map((listing) => (
+                    <ListingItems listing={listing} key={listing._id} />
                   ))
                 }
               </div>
@@ -95,7 +98,7 @@ export default function Home() {
           )
         }
         {
-          rentListings && rentListings.length>0 && (
+          rentListings && rentListings.length > 0 && (
             <div className="">
               <div className="my-3">
                 <h2 className='text-2xl font-semibold text-slate-600'>Recent places for rent</h2>
@@ -105,8 +108,8 @@ export default function Home() {
               </div>
               <div className="flex flex-wrap gap-4">
                 {
-                  rentListings. map((listing)=>(
-                    <ListingItems  listing={listing} key={listing._id}/>
+                  rentListings.map((listing) => (
+                    <ListingItems listing={listing} key={listing._id} />
                   ))
                 }
               </div>
@@ -114,7 +117,7 @@ export default function Home() {
           )
         }
         {
-          saleListings && saleListings.length>0 && (
+          saleListings && saleListings.length > 0 && (
             <div className="">
               <div className="my-3">
                 <h2 className='text-2xl font-semibold text-slate-600'>Recent places for sale</h2>
@@ -124,8 +127,8 @@ export default function Home() {
               </div>
               <div className="flex flex-wrap gap-4">
                 {
-                  saleListings. map((listing)=>(
-                    <ListingItems  listing={listing} key={listing._id}/>
+                  saleListings.map((listing) => (
+                    <ListingItems listing={listing} key={listing._id} />
                   ))
                 }
               </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,15 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://real-estate-project-server.onrender.com/",
-        changeOrigin: true, // Ensures the host header is modified for the proxy
-        secure: true,       // Set to true if the server uses HTTPS
-        rewrite: (path) => path.replace(/^\/api/, ""), // Optional: if the backend expects the `/api` path to be stripped
-      },
-    },
-  },
   plugins: [react()],
 });
